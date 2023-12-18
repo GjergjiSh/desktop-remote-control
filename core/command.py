bf5d7872e2592c792dfec2179eab4efbd5358fa4
GjergjiSh <gjergji_shkurti@yahoo.com>
2023-12-18 08:21:31 +0100

    introduce Result

--- core/command.py
+++ core/command.py
@@ -1,23 +1,113 @@
 from abc import ABC, abstractmethod
+from enum import Enum
+from concurrent.futures import Future
+from typing import Generic, TypeVar, Union, Optional
 
-class Command(ABC):
-    id: str
+T = TypeVar('T')
+E = TypeVar('E')
+
+class Result(Generic[T, E]):
+    def __init__(self, value: Optional[T] = None, error: Optional[E] = None):
+        self._value = value
+        self._error = error
+
+    @property
+    def value(self) -> Optional[T]:
+        return self._value
+
+    @property
+    def error(self) -> Optional[E]:
+        return self._error
+
+    def is_ok(self) -> bool:
+        return self._error is None
+
+    def is_error(self) -> bool:
+        return self._error is not None
+
+    def from_value(self, value: T) -> 'Result':
+        self._value = value
+        return self
+
+    def from_error(self, error: E) -> 'Result':
+        self._error = error
+        return self
+
+
+class ICommand(ABC):
+    @abstractmethod
+    def execute(self, *args, **kwargs) -> Result:
+        raise NotImplementedError
+
+class IAsyncCommand(ABC):
+    future: Future
 
     @abstractmethod
-    def execute(self, *args, **kwargs) -> int:
-        pass
+    async def execute(self, *args, **kwargs) -> Future:
+        raise NotImplementedError
 
+class BaseInvoker():
+    commands : dict[str,ICommand]
 
-class Invoker(ABC):
-    commands: dict[str, Command]
+    def __init__(self) -> None:
+        self.commands = {}
 
-    @abstractmethod
-    def invoke(self, command_id: str, *args, **kwargs) -> int:
-        pass
+    def invoke(self, command_id: str, *args, **kwargs) -> Result:
+        if command_id not in self.commands:
+            raise CommandError(
+                CommandErrorCode.INVALID_ARGUMENT,
+                f'Command {command_id} does not exist'
+            )
 
-    def add_command(self, command: Command) -> None:
-        self.commands[command.id] = command
+        return self.commands[command_id].execute(*args, **kwargs)
+
+    def add_command(self, command_id: str, command: ICommand) -> None:
+        self.commands[command_id] = command
 
     def remove_command(self, command_id: str) -> None:
-        if command_id in self.commands:
-            del self.commands[command_id]
+        if command_id not in self.commands:
+            raise CommandError(
+                CommandErrorCode.INVALID_ARGUMENT,
+                f'Command {command_id} does not exist'
+            )
+
+        del self.commands[command_id]
+
+class BaseAsyncInvoker():
+    commands : dict[str,IAsyncCommand]
+
+    def __init__(self) -> None:
+        self.commands = {}
+
+    async def invoke(self, command_id: str, *args, **kwargs) -> None:
+        if command_id not in self.commands:
+            raise CommandError(
+                CommandErrorCode.INVALID_ARGUMENT,
+                f'Command {command_id} does not exist'
+            )
+
+        self.commands[command_id].execute(*args, **kwargs)
+
+    def add_command(self, command_id: str, command: IAsyncCommand) -> None:
+        self.commands[command_id] = command
+
+    def remove_command(self, command_id: str) -> None:
+        if command_id not in self.commands:
+            raise CommandError(
+                CommandErrorCode.INVALID_ARGUMENT,
+                f'Command {command_id} does not exist'
+            )
+
+        del self.commands[command_id]
+
+class CommandErrorCode(Enum):
+    INVALID_ARGUMENT = 1
+
+class CommandError(Exception):
+    def __init__(self, domain: CommandErrorCode, message: str):
+        self.domain = domain
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.domain.name}: {self.message}'
--- core/system.py
+++ core/system.py
@@ -1,21 +1,48 @@
-from core.command import Command, Invoker
+from core.command import ICommand, BaseInvoker, CommandErrorCode, CommandError, Result
 import screen_brightness_control as sbc
 import os
+import platform
 
-class System(Invoker):
+class System(BaseInvoker):
     def __init__(self) -> None:
-        self.commands = {
+        self.COMMANDS = {
             "sleep": Sleep(),
             "shutdown": Shutdown(),
         }
 
-    def invoke(self, command_id: str, *args, **kwargs):
-        return self.commands[command_id].execute(*args, **kwargs)
+class Sleep(ICommand):
+    # The actual system calls being made via the os module
+    SLEEP_COMMANDS = {
+        "instant": {
+            "linux": "systemctl suspend",
+            "windows": "rundll32.exe powrprof.dll,SetSuspendState 0,1,0"
+        },
+        "deferred": {
+            "linux": "echo 'systemctl suspend' | at now + {} minutes",
+            "windows": "timeout /t {} /nobreak && rundll32.exe powrprof.dll,SetSuspendState 0,1,0"
+        }
+    }
 
-class Sleep(Command):
-    def execute(self, *args, **kwargs):
-        return os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+    def execute(self, *args, **kwargs) -> Result:
+        delay = kwargs.get('delay')
+        self._sleep(delay)
 
-class Shutdown(Command):
+    # Sets the delay to the system call string
+    # and executes it via the os module
+    def _sleep(self, delay=None):
+        os_type = self.get_os()
+        if delay is None:
+            command = self.SLEEP_COMMANDS['instant'][os_type]
+        else:
+            command = self.SLEEP_COMMANDS['deferred'][os_type] \
+            .format(delay * 60 if os_type == 'windows' else delay)
+
+        os.system(command)
+
+    @staticmethod
+    def get_os():
+        return 'windows' if platform.system() == 'Windows' else 'linux'
+
+class Shutdown(ICommand):
     def execute(self, *args, **kwargs):
         return os.system("shutdown /s /t 1")
